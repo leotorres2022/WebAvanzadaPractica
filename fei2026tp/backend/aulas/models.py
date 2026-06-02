d9c85fa6269e03a73bc309cd9ff604fba29382e5
leotorres <leo_escobar11@hotmail.com>
2026-06-01 22:24:20 -0300

    creacion de tablas relacionadas

--- fei2026tp/backend/aulas/models.py
+++ fei2026tp/backend/aulas/models.py
@@ -35,3 +35,22 @@ class Aula(models.Model):
 
     def __str__(self):
         return self.descripcion
+    
+class ReservaAula(models.Model):
+    id_aula = models.ForeignKey(Aula, on_delete=models.CASCADE, related_name='reservas')
+    fh_desde = models.DateTimeField()
+    fh_hasta = models.DateTimeField()
+    observacion = models.CharField(max_length=256)
+
+    def __str__(self):
+        return f"Reserva Aula {self.id_aula_id} ({self.fh_desde} - {self.fh_hasta})"
+
+
+class HorarioMateria(models.Model):
+    id_materia = models.ForeignKey(Materia, on_delete=models.CASCADE, related_name='horarios')
+    id_reserva = models.ForeignKey(ReservaAula, on_delete=models.CASCADE, related_name='horarios')
+    fh_desde = models.DateTimeField()
+    fh_hasta = models.DateTimeField()
+
+    def __str__(self):
+        return f"Horario Materia {self.id_materia_id} ({self.fh_desde})"
